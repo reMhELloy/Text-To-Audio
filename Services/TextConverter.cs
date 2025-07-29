f193cbff5719fce16a1decc40ac2b67efc5b9171
Nguyen Quang Cuong <cn228689@gmail.com>
2025-07-30 03:00:02 +0700

    Update

--- Services/TextConverter.cs
+++ Services/TextConverter.cs
@@ -11,22 +11,21 @@ namespace JapaneseConverter
         public static string ConvertToImgTags(string japaneseText)
         {
             var result = new StringBuilder();
-
             for (int i = 0; i < japaneseText.Length; i++)
             {
                 // Check for two-character combinations first (existing hiragana/katakana combo logic)
                 if (i < japaneseText.Length - 1)
                 {
                     string twoChars = japaneseText.Substring(i, 2);
-
                     if (JapanMapping.HiraganaComboMap.TryGetValue(twoChars, out var hiraganaCombo))
                     {
                         string firstChar = hiraganaCombo.Item1;
                         string secondChar = hiraganaCombo.Item2;
-
                         if (firstChar.StartsWith("dakuon_"))
                         {
+                            // Thêm cả PNG và GIF cho dakuon
                             result.Append($"<img src=\"hiragana_{firstChar}.png\">");
+                            result.Append($"<img src=\"{twoChars[0]}.gif\">");  // Dùng ký tự đầu của combo
                         }
                         else
                         {
@@ -36,15 +35,15 @@ namespace JapaneseConverter
                         i++; // Skip next character as it's part of the combo
                         continue;
                     }
-
                     if (JapanMapping.KatakanaComboMap.TryGetValue(twoChars, out var katakanaCombo))
                     {
                         string firstChar = katakanaCombo.Item1;
                         string secondChar = katakanaCombo.Item2;
-
                         if (firstChar.StartsWith("dakuon_"))
                         {
+                            // Thêm cả PNG và GIF cho dakuon
                             result.Append($"<img src=\"katakana_{firstChar}.png\">");
+                            result.Append($"<img src=\"{twoChars[0]}.gif\">");  // Dùng ký tự đầu của combo
                         }
                         else
                         {
@@ -68,11 +67,17 @@ namespace JapaneseConverter
                 // Existing conversion logic for hiragana and katakana
                 else if (JapanMapping.HiraganaDakuonMap.ContainsKey(c))
                 {
-                    result.Append($"<img src=\"hiragana_dakuon_{JapanMapping.HiraganaDakuonMap[c]}.png\">");
+                    // Thêm cả PNG và GIF cho Hiragana dakuon
+                    string dakuonValue = JapanMapping.HiraganaDakuonMap[c];
+                    result.Append($"<img src=\"hiragana_dakuon_{dakuonValue}.png\">");
+                    result.Append($"<img src=\"{c}.gif\">");  // Dùng ký tự gốc cho GIF
                 }
                 else if (JapanMapping.KatakanaDakuonMap.ContainsKey(c))
                 {
-                    result.Append($"<img src=\"katakana_dakuon_{JapanMapping.KatakanaDakuonMap[c]}.png\">");
+                    // Thêm cả PNG và GIF cho Katakana dakuon
+                    string dakuonValue = JapanMapping.KatakanaDakuonMap[c];
+                    result.Append($"<img src=\"katakana_dakuon_{dakuonValue}.png\">");
+                    result.Append($"<img src=\"{c}.gif\">");  // Dùng ký tự gốc cho GIF
                 }
                 else if (JapanMapping.KatakanaMap.ContainsKey(c))
                 {
@@ -87,7 +92,6 @@ namespace JapaneseConverter
                     result.Append(c);
                 }
             }
-
             return result.ToString();
         }
     }
